6016a22a6f5c87fe73c7ed1aaa88928e91dbd4bd
Ross Cameron <rossc579@gmail.com>
2025-12-28 21:13:34 +0000

    add delay line to sum into the filter

--- play_audio.cpp
+++ play_audio.cpp
@@ -73,6 +73,28 @@ void playback_thread_func(ALSAPlaybackDevice* speaker, AudioQueue* audio_queue) 
     }
 }
 
+class DelayLine {
+public:
+    explicit DelayLine(size_t delay)
+        : buffer(delay, 0.0), size(delay) {}
+
+    inline double process(double x) {
+        double y = buffer[index];
+        buffer[index] = x;
+
+        index++;
+        if (index == size) index = 0;
+
+        return y;
+    }
+
+private:
+    std::vector<double> buffer;
+    size_t size;
+    size_t index = 0;
+};
+
+
 
 int main() {
     const snd_pcm_format_t FORMAT = SND_PCM_FORMAT_S16_LE;
@@ -90,6 +112,10 @@ int main() {
     
     Fir1 fir(NTAPS,0.00000);
     fir.setLearningRate(LEARNING_RATE);
+    size_t delay_line_length = NTAPS/2;
+
+    DelayLine delay_line(delay_line_length);
+
     while (1) {
         AudioMessage in_msg = DSP_receiving_queue.pop();
         AudioMessage out_msg(speaker.get_frames_per_period(),speaker.get_bytes_per_frame());
@@ -108,25 +134,29 @@ int main() {
                 double sum  = static_cast<double>(sum32 >> 1);
                 double diff = static_cast<double>(diff32 >> 1);
                 
-
+                double delayed_sum = delay_line.process(sum);
                 double canceller = fir.filter(diff);
                 if(std::abs(canceller)>65536){
                     fir.reset();
                     out[i]= static_cast<int16_t>(sum)*GAIN;
                     continue;
                 }
-                double output = sum - canceller;
+
+                double output = delayed_sum - canceller;
                 if (output<-32768){
                     output = -32768;
                 }
                 if(output>32767){
                     output = 32767;
                 }
+
                 fir.lms_update(output);
                 
                 int16_t out16 = static_cast<int16_t>(output);
 
                 out[i]= out16*GAIN;
+
+                
                 }
         }
         DSP_transmission_queue.push(std::move(out_msg));
