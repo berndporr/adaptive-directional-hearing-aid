7d10e1ede57a843ce6beb492d8b4ee0d54963694
Bernd Porr <mail@berndporr.me.uk>
2026-05-16 21:07:46 +0100

    Better clang format. Added buffer resizing.

--- ALSADevices.cpp
+++ ALSADevices.cpp
@@ -10,38 +10,85 @@ bool ALSAPCMDevice::open ()
     int rc = snd_pcm_open (&handle, device_name.c_str (),
                            get_pcm_stream_type (), 0);
     if (rc < 0)
-        {
-            fprintf (stderr, "unable to open pcm device: %s\n",
-                     snd_strerror (rc));
-            return false;
-        }
+    {
+        fprintf (stderr, "unable to open pcm device: %s\n", snd_strerror (rc));
+        return false;
+    }
 
     snd_pcm_hw_params_t *params;
 
     /* Allocate a hardware parameters object. */
     snd_pcm_hw_params_alloca (&params);
 
-    snd_pcm_hw_params_any (handle, params);
+    rc = snd_pcm_hw_params_any (handle, params);
+    if (rc < 0)
+    {
+        fprintf (stderr, "Broken configuration: %s.\n", snd_strerror (rc));
+        return false;
+    }
 
-    snd_pcm_hw_params_set_access (handle, params,
-                                  SND_PCM_ACCESS_RW_INTERLEAVED);
+    rc = snd_pcm_hw_params_set_access (handle, params,
+                                       SND_PCM_ACCESS_RW_INTERLEAVED);
+    if (rc < 0)
+    {
+        fprintf (stderr, "Can't set access: %s.\n", snd_strerror (rc));
+        return false;
+    }
 
-    snd_pcm_hw_params_set_format (handle, params, format);
+    rc = snd_pcm_hw_params_set_format (handle, params, format);
+    if (rc < 0)
+    {
+        fprintf (stderr, "Can't set format: %s.\n", snd_strerror (rc));
+        return false;
+    }
 
-    snd_pcm_hw_params_set_channels (handle, params, channels);
+    rc = snd_pcm_hw_params_set_channels (handle, params, channels);
+    if (rc < 0)
+    {
+        fprintf (stderr, "Can't set channels: %s.\n", snd_strerror (rc));
+        return false;
+    }
 
-    snd_pcm_hw_params_set_rate (handle, params, sample_rate, 0);
+    rc = snd_pcm_hw_params_set_rate (handle, params, sample_rate, 0);
+    if (rc < 0)
+    {
+        fprintf (stderr, "Can't set sampling rate: %s.\n", snd_strerror (rc));
+        return false;
+    }
 
-    snd_pcm_hw_params_set_period_size (
+    rc = snd_pcm_hw_params_set_period_size (
         handle, params, static_cast<snd_pcm_uframes_t> (frames_per_period), 0);
+    if (rc < 0)
+    {
+        fprintf (stderr, "Can't set period size: %s.\n", snd_strerror (rc));
+        return false;
+    }
+
+    rc = snd_pcm_hw_params_set_buffer_size (
+        handle, params,
+        static_cast<snd_pcm_uframes_t> (frames_per_period * 2));
+    if (rc < 0)
+    {
+        fprintf (stderr, "Can't set buffer size: %s.\n", snd_strerror (rc));
+        return false;
+    }
 
     rc = snd_pcm_hw_params (handle, params);
     if (rc < 0)
-        {
-            fprintf (stderr, "unable to set hw parameters: %s\n",
-                     snd_strerror (rc));
-            return false;
-        }
+    {
+        fprintf (stderr, "unable to set hw parameters: %s\n",
+                 snd_strerror (rc));
+        return false;
+    }
+
+    unsigned int ppbuf;
+    snd_pcm_hw_params_get_periods_min (params, &ppbuf, NULL);
+    if (ppbuf > 2)
+    {
+        fprintf (stderr,
+                 "Playback device does not support 2 periods per buffer.\n");
+        return false;
+    }
 
     snd_pcm_hw_params_get_period_time (params, &period_time, 0);
 
@@ -58,13 +105,13 @@ void ALSACaptureDevice::worker ()
     buffer.resize (frames_per_period * channels);
     running = true;
     while (running)
+    {
+        capture (buffer);
+        if (onPeriod)
         {
-            capture (buffer);
-            if (onPeriod)
-                {
-                    onPeriod (buffer);
-                }
+            onPeriod (buffer);
         }
+    }
 }
 
 void ALSAPCMDevice::close ()
@@ -90,9 +137,9 @@ bool ALSACaptureDevice::open ()
 {
     bool b = ALSAPCMDevice::open ();
     if (!b)
-        {
-            return b;
-        }
+    {
+        return b;
+    }
     thr = std::thread (&ALSACaptureDevice::worker, this);
     return b;
 }
@@ -113,9 +160,9 @@ snd_pcm_sframes_t ALSACaptureDevice::capture (std::vector<int16_t> &buffer)
                          static_cast<snd_pcm_uframes_t> (frames_per_period));
 
     if (frames_read < 0)
-        {
-            snd_pcm_recover (handle, static_cast<int> (frames_read), 1);
-        }
+    {
+        snd_pcm_recover (handle, static_cast<int> (frames_read), 1);
+    }
 
     return frames_read;
 }
@@ -130,10 +177,10 @@ ALSAPlaybackDevice::onPeriod (const std::vector<int16_t> &period)
                           static_cast<snd_pcm_uframes_t> (frames_per_period));
 
     if (frames_written == -EPIPE)
-        {
-            /* EPIPE means underrun */
-            snd_pcm_prepare (handle);
-        }
+    {
+        /* EPIPE means underrun */
+        snd_pcm_prepare (handle);
+    }
 
     return frames_written;
 }
